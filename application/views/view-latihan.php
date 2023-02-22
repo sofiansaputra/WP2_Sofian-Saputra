80516ae6711caf59f2f131162477768fe51ab093
WP2_Sofian Saputra <101610331+sofiansaputra@users.noreply.github.com>
2023-02-22 07:49:20 +0700

    Add files via upload

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming.!!<br>
+    Nilai = <?= $nilai1; ?>
+    Nilai = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+                                                                $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
